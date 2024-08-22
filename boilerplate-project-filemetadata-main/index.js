3dcf436cd54a4d5b23269acbbc737f9cb9c8cdb1
Kevin <kramzyvine@icloud.com>
2024-08-22 19:52:40 +0300

    Completed project!

--- boilerplate-project-filemetadata-main/index.js
+++ boilerplate-project-filemetadata-main/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+let upload = multer({ dest: "uploads/" });
+const bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
